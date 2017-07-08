c5ee051cef1352bcb3dfe2b571b01859cdb64b33
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-07-08 17:10:16 -0400

    Added ctor to Vector3.

--- Code/Engine/Source/Math/Vector3.cs
+++ Code/Engine/Source/Math/Vector3.cs
@@ -8,8 +8,23 @@ namespace Entmoot.Engine
 {
 	public struct Vector3
 	{
+		#region Fields
+
 		public float X;
 		public float Y;
 		public float Z;
+
+		#endregion Fields
+
+		#region Constructors
+
+		public Vector3(float x, float y, float z)
+		{
+			this.X = x;
+			this.Y = y;
+			this.Z = z;
+		}
+
+		#endregion Constructors
 	}
 }
