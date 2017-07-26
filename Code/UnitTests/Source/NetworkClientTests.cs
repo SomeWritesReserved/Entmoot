9e7b2bd6f194b587414abe3786f1c96988570141
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-07-26 00:48:45 -0400

    Refactored unit tests to make it easier to control when mocked packets come in.

--- Code/UnitTests/Source/NetworkClientTests.cs
+++ Code/UnitTests/Source/NetworkClientTests.cs
@@ -21,8 +21,8 @@ namespace Entmoot.UnitTests
 		[Test]
 		public void Client_RenderStateInterpolation()
 		{
-			Client client = NetworkClientTests.createDefaultTestCase();
-			client.ShouldInterpolate = true;
+			MockClient client = NetworkClientTests.createTestCase1();
+			client.EngineClient.ShouldInterpolate = true;
 			NetworkClientTests.updateClientAndAssertState(client, 1, 1, false, null);
 			NetworkClientTests.updateClientAndAssertState(client, 2, 1, false, null);
 			NetworkClientTests.updateClientAndAssertState(client, 3, 1, false, null);
@@ -58,8 +58,8 @@ namespace Entmoot.UnitTests
 		[Test]
 		public void Client_RenderStateNoInterpolation()
 		{
-			Client client = NetworkClientTests.createDefaultTestCase();
-			client.ShouldInterpolate = false;
+			MockClient client = NetworkClientTests.createTestCase1();
+			client.EngineClient.ShouldInterpolate = false;
 			NetworkClientTests.updateClientAndAssertState(client, 1, 1, false, null);
 			NetworkClientTests.updateClientAndAssertState(client, 2, 1, false, null);
 			NetworkClientTests.updateClientAndAssertState(client, 3, 1, false, null);
@@ -96,37 +96,36 @@ namespace Entmoot.UnitTests
 
 		#region Helpers
 
-		private static void updateClientAndAssertState(Client client, int clientFrameTick, int recievedServerFrameTick, bool hasInterpStarted, float? position, int extrapolatedFrames = 0, int noInterpFrames = 0)
+		private static void updateClientAndAssertState(MockClient mockClient, int clientFrameTick, int recievedServerFrameTick, bool hasInterpStarted, float? position, int extrapolatedFrames = 0, int noInterpFrames = 0)
 		{
-			client.Update(CommandKeys.None);
-			Assert.AreEqual(clientFrameTick, client.FrameTick, "FrameTick_" + clientFrameTick);
-			Assert.AreEqual(recievedServerFrameTick, client.LatestReceivedServerTick, "LatestReceivedServerTick_" + clientFrameTick);
-			Assert.AreEqual(hasInterpStarted, client.HasInterpolationStarted, "HasInterpolationStarted_" + clientFrameTick);
-			Assert.AreEqual(hasInterpStarted, client.InterpolationStartState != null, "InterpolationStartState_" + clientFrameTick);
-			Assert.AreEqual(hasInterpStarted, client.InterpolationEndState != null, "InterpolationEndState_" + clientFrameTick);
-			Assert.AreEqual(extrapolatedFrames, client.NumberOfExtrapolatedFrames, "NumberOfExtrapolatedFrames_" + clientFrameTick);
-			Assert.AreEqual(noInterpFrames, client.NumberOfNoInterpolationFrames, "NumberOfNoInterpolationFrames_" + clientFrameTick);
-			Assert.AreEqual(position.HasValue, client.RenderedState != null, "RenderedState_" + clientFrameTick);
+			mockClient.Update(CommandKeys.None);
+			Client engineClient = mockClient.EngineClient;
+			Assert.AreEqual(clientFrameTick, engineClient.FrameTick, "FrameTick_" + clientFrameTick);
+			Assert.AreEqual(recievedServerFrameTick, engineClient.LatestReceivedServerTick, "LatestReceivedServerTick_" + clientFrameTick);
+			Assert.AreEqual(hasInterpStarted, engineClient.HasInterpolationStarted, "HasInterpolationStarted_" + clientFrameTick);
+			Assert.AreEqual(hasInterpStarted, engineClient.InterpolationStartState != null, "InterpolationStartState_" + clientFrameTick);
+			Assert.AreEqual(hasInterpStarted, engineClient.InterpolationEndState != null, "InterpolationEndState_" + clientFrameTick);
+			Assert.AreEqual(extrapolatedFrames, engineClient.NumberOfExtrapolatedFrames, "NumberOfExtrapolatedFrames_" + clientFrameTick);
+			Assert.AreEqual(noInterpFrames, engineClient.NumberOfNoInterpolationFrames, "NumberOfNoInterpolationFrames_" + clientFrameTick);
+			Assert.AreEqual(position.HasValue, engineClient.RenderedState != null, "RenderedState_" + clientFrameTick);
 			if (position.HasValue)
 			{
-				Assert.AreEqual(position.Value, client.RenderedState.Entities[0].Position.X, 0.001f, "RenderedState_" + clientFrameTick);
+				Assert.AreEqual(position.Value, engineClient.RenderedState.Entities[0].Position.X, 0.001f, "RenderedState_" + clientFrameTick);
 			}
 		}
 
-		private static Client createDefaultTestCase()
+		private static MockClient createTestCase1()
 		{
-			MockServerNetworkConnection networkConnection = new MockServerNetworkConnection();
-			networkConnection.SimulateTick(1, new Vector3(10, 0, 0));
-			networkConnection.SimulateTick(4, new Vector3(10, 0, 0));
-			networkConnection.SimulateTick(7, new Vector3(10, 0, 0));
-			networkConnection.SimulateTick(10, new Vector3(20, 0, 0));
-			networkConnection.SimulateTick(13, new Vector3(30, 0, 0));
-			networkConnection.SimulateTick(16, new Vector3(35, 0, 0));
-			Client client = new Client(networkConnection);
-			client.InterpolationRenderDelay = 8;
-			client.MaxExtrapolationTicks = 3;
-			client.ShouldPredictInput = false;
-			networkConnection.OwnerClient = client;
+			MockClient client = MockClient.CreateMockClient();
+			client.EngineClient.InterpolationRenderDelay = 8;
+			client.EngineClient.MaxExtrapolationTicks = 3;
+			client.EngineClient.ShouldPredictInput = false;
+			client.QueueIncomingStateUpdate(1, 1, new Vector3(10, 0, 0));
+			client.QueueIncomingStateUpdate(4, 4, new Vector3(10, 0, 0));
+			client.QueueIncomingStateUpdate(7, 7, new Vector3(10, 0, 0));
+			client.QueueIncomingStateUpdate(10, 10, new Vector3(20, 0, 0));
+			client.QueueIncomingStateUpdate(13, 13, new Vector3(30, 0, 0));
+			client.QueueIncomingStateUpdate(16, 16, new Vector3(35, 0, 0));
 			return client;
 		}
 
@@ -134,7 +133,22 @@ namespace Entmoot.UnitTests
 
 		#region Nested Types
 
-		private class MockServerNetworkConnection : INetworkConnection
+		/// <summary>
+		/// Represents a mock client that is "connected" to a server (data incoming from the server is, outgoing data is not mocked).
+		/// Use this instead of using a <see cref="Client"/> object directly (since this offers deterministic simulated packet arrival).
+		/// Add mocked packets by calling <see cref="QueueIncomingStateUpdate"/> and they will "arrive" on the pre-determined tick you specify.
+		/// </summary>
+		/// <remarks>
+		/// This wraps a <see cref="Client"/> object and you should call <see cref="Update"/> on this object rather than on the wrapped
+		/// up <see cref="Client"/> object. This is because this class keeps track of its own ticks in order to simulate packet arrivals
+		/// in a sane way (which isn't possible if we based off of <see cref="Client"/>'s ticks since it changes its tick as needed
+		/// to sync with the server). With this class, the tick is always monotonically increasing by one every call to <see cref="Update"/>.
+		/// This class also happens to be the <see cref="INetworkConnection"/> for the wrapped <see cref="Client"/> which makes a weird
+		/// circular reference but this is the easiest way to control exactly when the packets come up independent of the <see cref="Client"/>'s
+		/// tick. So even though this is a <see cref="INetworkConnection"/> it should be treated as a client (hence the name <see cref="MockClient"/>
+		/// rather than something like MockedClientNetworkConnection).
+		/// </remarks>
+		private class MockClient : INetworkConnection
 		{
 			#region Fields
 
@@ -144,34 +158,64 @@ namespace Entmoot.UnitTests
 
 			#region Properties
 
-			public Client OwnerClient { get; set; }
+			/// <summary>Gets the current tick of the network, independent of the underlying <see cref="Client"/>.</summary>
+			public int NetworkTick { get; private set; }
+
+			/// <summary>Gets the underlying <see cref="Client"/> object.</summary>
+			public Client EngineClient { get; private set; }
 
 			#endregion Properties
 
 			#region Methods
 
-			public void SimulateTick(int arrivalFrameTick, Vector3 entityPosition)
+			/// <summary>
+			/// Returns a newly created <see cref="MockClient"/> object along with an underlying <see cref="Client"/>, all ready to go.
+			/// </summary>
+			public static MockClient CreateMockClient()
 			{
-				if (!this.queuedStateSnapshots.ContainsKey(arrivalFrameTick))
+				MockClient mockClient = new MockClient();
+				Client engineClient = new Client(mockClient);
+				mockClient.EngineClient = engineClient;
+				return mockClient;
+			}
+
+			/// <summary>
+			/// Updates the state of the network and will also update the underlying <see cref="Client"/> object. It is important
+			/// to call this <see cref="Update"/> rather than <see cref="Client.Update"/> otherwise the underlying <see cref="Client"/>
+			/// will never get new packets (since the network never gets updated).
+			/// </summary>
+			public void Update(CommandKeys activeCommandKeys)
+			{
+				this.NetworkTick++;
+				this.EngineClient.Update(activeCommandKeys);
+			}
+
+			/// <summary>
+			/// Creates and adds a new <see cref="StateSnapshot"/> to the network that will "arrive" for the client at a specified network tick
+			/// (i.e. after that many calls to <see cref="Update"/> the given <see cref="StateSnapshot"/> packet will "arrive" for the underlying
+			/// <see cref="Client"/>).
+			/// </summary>
+			public void QueueIncomingStateUpdate(int arrivalNetworkTick, int serverFrameTick, Vector3 entityPosition)
+			{
+				if (!this.queuedStateSnapshots.ContainsKey(arrivalNetworkTick))
 				{
-					this.queuedStateSnapshots[arrivalFrameTick] = new Queue<StateSnapshot>();
+					this.queuedStateSnapshots[arrivalNetworkTick] = new Queue<StateSnapshot>();
 				}
 				StateSnapshot stateSnapshot = new StateSnapshot()
 				{
-					ServerFrameTick = arrivalFrameTick,
+					ServerFrameTick = serverFrameTick,
 					Entities = new Entity[] { new Entity() { Position = entityPosition } },
 				};
-				this.queuedStateSnapshots[arrivalFrameTick].Enqueue(stateSnapshot);
+				this.queuedStateSnapshots[arrivalNetworkTick].Enqueue(stateSnapshot);
 			}
 
-			public byte[] GetNextIncomingPacket()
+			byte[] INetworkConnection.GetNextIncomingPacket()
 			{
-				int clientFrameTick = this.OwnerClient.FrameTick;
-				if (!this.queuedStateSnapshots.ContainsKey(clientFrameTick) || !this.queuedStateSnapshots[clientFrameTick].Any()) { return null; }
-				return this.queuedStateSnapshots[clientFrameTick].Dequeue().SerializePacket();
+				if (!this.queuedStateSnapshots.ContainsKey(this.NetworkTick) || !this.queuedStateSnapshots[this.NetworkTick].Any()) { return null; }
+				return this.queuedStateSnapshots[this.NetworkTick].Dequeue().SerializePacket();
 			}
 
-			public void SendPacket(byte[] packet)
+			void INetworkConnection.SendPacket(byte[] packet)
 			{
 				// Do nothing, the other endpoint doesn't exist so it won't respond to anything anyway
 			}
