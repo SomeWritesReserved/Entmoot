23b4cb590d8ba6b55fba5d6882a63857815401c5
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-07-30 21:45:39 -0400

    Added testcase2 unit tests

--- Code/UnitTests/Source/NetworkClientTests.cs
+++ Code/UnitTests/Source/NetworkClientTests.cs
@@ -14,7 +14,7 @@ namespace Entmoot.UnitTests
 		#region Tests
 
 		[Test]
-		public void Client_FirstConnect()
+		public void FirstConnect()
 		{
 			MockClient client = NetworkClientTests.createTestCase0();
 			client.EngineClient.ShouldInterpolate = true;
@@ -50,7 +50,7 @@ namespace Entmoot.UnitTests
 		}
 
 		[Test]
-		public void Client_RenderStateInterpolation()
+		public void RenderState1_Interpolation()
 		{
 			MockClient client = NetworkClientTests.createTestCase1();
 			client.EngineClient.ShouldInterpolate = true;
@@ -88,7 +88,7 @@ namespace Entmoot.UnitTests
 		}
 
 		[Test]
-		public void Client_RenderStateNoInterpolation()
+		public void RenderState1_NoInterpolation()
 		{
 			MockClient client = NetworkClientTests.createTestCase1();
 			client.EngineClient.ShouldInterpolate = false;
@@ -123,6 +123,54 @@ namespace Entmoot.UnitTests
 			NetworkClientTests.updateClientAndAssertState(client, 28, 16, true, 35.0f);
 			NetworkClientTests.updateClientAndAssertState(client, 29, 16, true, 35.0f);
 			NetworkClientTests.updateClientAndAssertState(client, 30, 16, true, 35.0f);
+		}
+
+		[Test]
+		public void RenderState2_Interpolation()
+		{
+			MockClient client = NetworkClientTests.createTestCase2();
+			client.EngineClient.ShouldInterpolate = true;
+			client.EngineClient.InterpolationRenderDelay = 8;
+			NetworkClientTests.updateClientAndAssertState(client, 1, 1, false, null);
+			NetworkClientTests.updateClientAndAssertState(client, 2, 1, false, null);
+			NetworkClientTests.updateClientAndAssertState(client, 3, 1, false, null);
+			NetworkClientTests.updateClientAndAssertState(client, 4, 4, false, null);
+			NetworkClientTests.updateClientAndAssertState(client, 5, 4, false, null);
+			NetworkClientTests.updateClientAndAssertState(client, 6, 4, false, null);
+			NetworkClientTests.updateClientAndAssertState(client, 7, 7, false, null);
+			NetworkClientTests.updateClientAndAssertState(client, 8, 7, false, null);
+			NetworkClientTests.updateClientAndAssertState(client, 9, 7, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 10, 10, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 11, 10, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 12, 10, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 13, 13, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 14, 13, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 15, 13, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 16, 16, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 17, 16, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 18, 16, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 19, 19, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 20, 19, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 21, 19, true, 10.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 22, 22, true, 11.6666f);
+			NetworkClientTests.updateClientAndAssertState(client, 23, 22, true, 13.3333f);
+			NetworkClientTests.updateClientAndAssertState(client, 24, 22, true, 15.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 25, 22, true, 18.3333f);
+			NetworkClientTests.updateClientAndAssertState(client, 26, 22, true, 21.6666f);
+			NetworkClientTests.updateClientAndAssertState(client, 27, 22, true, 25.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 28, 22, true, 26.6666f);
+			NetworkClientTests.updateClientAndAssertState(client, 29, 22, true, 28.3333f);
+			NetworkClientTests.updateClientAndAssertState(client, 30, 22, true, 30.0f);
+			NetworkClientTests.updateClientAndAssertState(client, 31, 22, true, 31.6666f, extrapolatedFrames: 1);
+			NetworkClientTests.updateClientAndAssertState(client, 32, 22, true, 33.3333f, extrapolatedFrames: 2);
+			NetworkClientTests.updateClientAndAssertState(client, 33, 22, true, 35.0f, extrapolatedFrames: 3);
+			NetworkClientTests.updateClientAndAssertState(client, 34, 22, true, 35.0f, extrapolatedFrames: 3, noInterpFrames: 1);
+			NetworkClientTests.updateClientAndAssertState(client, 35, 22, true, 35.0f, extrapolatedFrames: 3, noInterpFrames: 2);
+		}
+
+		[Test]
+		public void Prediction()
+		{
 		}
 
 		#endregion Tests
@@ -180,6 +228,27 @@ namespace Entmoot.UnitTests
 			client.QueueIncomingStateUpdate(10, 10, 20.0f);
 			client.QueueIncomingStateUpdate(13, 13, 30.0f);
 			client.QueueIncomingStateUpdate(16, 16, 35.0f);
+			return client;
+		}
+
+		/// <summary>
+		/// Creates and returns a standard test case of incoming packets; simulates 2 tick latecy to server, 3 tick server network update rate,
+		/// but doesn't simulate connecting (i.e. mock a mid-stream connection). No packet jitter. Mock server acknowledgements of client commands.
+		/// </summary>
+		private static MockClient createTestCase2()
+		{
+			MockClient client = MockClient.CreateMockClient();
+			client.EngineClient.InterpolationRenderDelay = 8;
+			client.EngineClient.MaxExtrapolationTicks = 3;
+			client.EngineClient.ShouldPredictInput = false;
+			client.QueueIncomingStateUpdate(1, 1, -1, 10.0f);
+			client.QueueIncomingStateUpdate(4, 4, -1, 10.0f);
+			client.QueueIncomingStateUpdate(7, 7, 3, 10.0f);
+			client.QueueIncomingStateUpdate(10, 10, 6, 10.0f);
+			client.QueueIncomingStateUpdate(13, 13, 9, 10.0f);
+			client.QueueIncomingStateUpdate(16, 16, 12, 15.0f);
+			client.QueueIncomingStateUpdate(19, 19, 15, 25.0f);
+			client.QueueIncomingStateUpdate(22, 22, 18, 30.0f);
 			return client;
 		}
 
