53c47a3db0bd9cdfbf6fcab73170c755c7fc5b5c
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-08-16 21:24:58 -0400

    Added unit tests for EntitySystem

---
+++ Code/UnitTests/Source/EntityManagerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entmoot.Engine;
+using NUnit.Framework;
+
+namespace Entmoot.UnitTests
+{
+	[TestFixture]
+	public class EntityManagerTests
+	{
+		#region Tests
+
+		[Test]
+		public void EntityCreate()
+		{
+			EntityManager entityManager = new EntityManager(4, new IEntitySystem[0]);
+			Entity newEntity = entityManager.CreateEntity<Entity>();
+			Assert.NotNull(newEntity);
+			Assert.AreEqual(0, newEntity.ID);
+			Assert.AreEqual(EntityState.Creating, newEntity.EntityState);
+			Assert.AreEqual(0, entityManager.Entities.Count);
+			entityManager.Update();
+			Assert.AreEqual(EntityState.Active, newEntity.EntityState);
+			Assert.AreEqual(newEntity, entityManager.Entities[0]);
+		}
+
+		[Test]
+		public void EntityCreateMultiple()
+		{
+			EntityManager entityManager = new EntityManager(4, new IEntitySystem[0]);
+			Entity newEntity1 = entityManager.CreateEntity<Entity>();
+			Assert.NotNull(newEntity1);
+			Assert.AreEqual(0, newEntity1.ID);
+			Assert.AreEqual(EntityState.Creating, newEntity1.EntityState);
+			Assert.AreEqual(0, entityManager.Entities.Count);
+			Entity newEntity2 = entityManager.CreateEntity<Entity>();
+			Assert.NotNull(newEntity2);
+			Assert.AreEqual(1, newEntity2.ID);
+			Assert.AreEqual(EntityState.Creating, newEntity2.EntityState);
+			Assert.AreEqual(0, entityManager.Entities.Count);
+			entityManager.Update();
+			Assert.AreEqual(EntityState.Active, newEntity1.EntityState);
+			Assert.AreEqual(newEntity1, entityManager.Entities[0]);
+			Assert.AreEqual(EntityState.Active, newEntity2.EntityState);
+			Assert.AreEqual(newEntity2, entityManager.Entities[1]);
+		}
+		
+		[Test]
+		public void EntityCreateMultiple_TooMany()
+		{
+		}
+
+		[Test]
+		public void EntityRemove()
+		{
+		}
+
+		[Test]
+		public void EntityRemoveRedundant()
+		{
+		}
+
+		[Test]
+		public void EntityCreateAndRemoveInSameTick()
+		{
+		}
+
+		#endregion Tests
+	}
+}
