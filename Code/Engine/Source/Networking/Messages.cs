a07bf1da24b5862d20f332f0c40e0bd60094c1be
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-09-21 21:46:01 -0400

    Added network messages for read/write operations

---
+++ Code/Engine/Source/Networking/Messages.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entmoot.Engine
+{
+	/// <summary>
+	/// Represents a message that can be read from which has come in from another endpoint.
+	/// </summary>
+	public class IncomingMessage
+	{
+		#region Fields
+
+		private readonly byte[] messageData;
+		private int dataIndex = 0;
+
+		#endregion Fields
+
+		#region Constructors
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public IncomingMessage(byte[] messageData)
+		{
+			this.messageData = messageData;
+		}
+
+		#endregion Constructors
+
+		#region Methods
+
+		/// <summary>
+		/// Reads an unsigned byte from the message.
+		/// </summary>
+		public byte ReadByte()
+		{
+			byte value = this.messageData[this.dataIndex];
+			this.dataIndex += sizeof(byte);
+			return value;
+		}
+
+		/// <summary>
+		/// Reads a signed 16-bit integer from the message.
+		/// </summary>
+		public short ReadInt16()
+		{
+			short value = (short)((this.messageData[this.dataIndex + 0] << 8) |
+				this.messageData[this.dataIndex + 1]);
+			this.dataIndex += sizeof(short);
+			return value;
+		}
+
+		/// <summary>
+		/// Reads an unsigned 16-bit integer from the message.
+		/// </summary>
+		public ushort ReadUInt16()
+		{
+			ushort value = (ushort)((this.messageData[this.dataIndex + 0] << 8) |
+				this.messageData[this.dataIndex + 1]);
+			this.dataIndex += sizeof(ushort);
+			return value;
+		}
+
+		/// <summary>
+		/// Reads a signed 32-bit integer from the message.
+		/// </summary>
+		public int ReadInt32()
+		{
+			int value = (int)((this.messageData[this.dataIndex + 0] << 24) |
+				(this.messageData[this.dataIndex + 1] << 16) |
+				(this.messageData[this.dataIndex + 2] << 8) |
+				this.messageData[this.dataIndex + 3]);
+			this.dataIndex += sizeof(int);
+			return value;
+		}
+
+		/// <summary>
+		/// Reads an unsigned 32-bit integer from the message.
+		/// </summary>
+		public uint ReadUInt32()
+		{
+			uint value = (uint)((this.messageData[this.dataIndex + 0] << 24) |
+				(this.messageData[this.dataIndex + 1] << 16) |
+				(this.messageData[this.dataIndex + 2] << 8) |
+				this.messageData[this.dataIndex + 3]);
+			this.dataIndex += sizeof(uint);
+			return value;
+		}
+
+		/// <summary>
+		/// Reads a 32-bit floating point number from the message.
+		/// </summary>
+		public float ReadSingle()
+		{
+			SingleToUIntUnion singleToUIntUnion = new SingleToUIntUnion();
+			singleToUIntUnion.UIntValue = this.ReadUInt32();
+			return singleToUIntUnion.SingleValue;
+		}
+
+		/// <summary>
+		/// Reads a boolean value from the message.
+		/// </summary>
+		public bool ReadBoolean()
+		{
+			return (this.ReadByte() == 1);
+		}
+
+		#endregion Methods
+	}
+
+	/// <summary>
+	/// Represents a message which can be written to that will go out to other endpoints
+	/// </summary>
+	public class OutgoingMessage
+	{
+		#region Fields
+
+		private readonly byte[] messageData;
+		private int dataIndex = 0;
+
+		#endregion Fields
+
+		#region Constructors
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public OutgoingMessage(byte[] messageData)
+		{
+			this.messageData = messageData;
+		}
+
+		#endregion Constructors
+
+		#region Methods
+
+		/// <summary>
+		/// Writes an unsigned byte to the message.
+		/// </summary>
+		public void Write(byte value)
+		{
+			this.messageData[this.dataIndex] = value;
+			this.dataIndex += sizeof(byte);
+		}
+
+		/// <summary>
+		/// Writes an signed 16-bit integer to the message.
+		/// </summary>
+		public void Write(short value)
+		{
+			this.messageData[this.dataIndex + 0] = (byte)(value >> 8);
+			this.messageData[this.dataIndex + 1] = (byte)value;
+			this.dataIndex += sizeof(short);
+		}
+
+		/// <summary>
+		/// Writes an unsigned 16-bit integer to the message.
+		/// </summary>
+		public void Write(ushort value)
+		{
+			this.messageData[this.dataIndex + 0] = (byte)(value >> 8);
+			this.messageData[this.dataIndex + 1] = (byte)value;
+			this.dataIndex += sizeof(ushort);
+		}
+
+		/// <summary>
+		/// Writes an signed 32-bit integer to the message.
+		/// </summary>
+		public void Write(int value)
+		{
+			this.messageData[this.dataIndex + 0] = (byte)(value >> 24);
+			this.messageData[this.dataIndex + 1] = (byte)(value >> 16);
+			this.messageData[this.dataIndex + 2] = (byte)(value >> 8);
+			this.messageData[this.dataIndex + 3] = (byte)value;
+			this.dataIndex += sizeof(int);
+		}
+
+		/// <summary>
+		/// Writes an unsigned 32-bit integer to the message.
+		/// </summary>
+		public void Write(uint value)
+		{
+			this.messageData[this.dataIndex + 0] = (byte)(value >> 24);
+			this.messageData[this.dataIndex + 1] = (byte)(value >> 16);
+			this.messageData[this.dataIndex + 2] = (byte)(value >> 8);
+			this.messageData[this.dataIndex + 3] = (byte)value;
+			this.dataIndex += sizeof(uint);
+		}
+
+		/// <summary>
+		/// Writes a 32-bit floating point number to the message.
+		/// </summary>
+		public void Write(float value)
+		{
+			SingleToUIntUnion singleToUIntUnion = new SingleToUIntUnion()
+			{
+				SingleValue = value,
+			};
+			this.Write(singleToUIntUnion.UIntValue);
+		}
+
+		/// <summary>
+		/// Writes a boolean value to the message.
+		/// </summary>
+		public void Write(bool value)
+		{
+			this.Write(value ? (byte)1 : (byte)0);
+		}
+
+		#endregion Methods
+	}
+
+	/// <summary>Convenience type to convert floats to ints (and vice-versa) byte for byte.</summary>
+	[StructLayout(LayoutKind.Explicit)]
+	internal struct SingleToUIntUnion
+	{
+		/// <summary>The value as a 32-bit floating point number.</summary>
+		[FieldOffset(0)]
+		public float SingleValue;
+
+		/// <summary>The value as an unsigned 32-bit integer.</summary>
+		[FieldOffset(0)]
+		public uint UIntValue;
+	}
+}
---
+++ Code/UnitTests/Source/NetMessageTests.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entmoot.Engine;
+using NUnit.Framework;
+
+namespace Entmoot.UnitTests
+{
+	[TestFixture]
+	public class NetMessageTests
+	{
+		#region Tests
+
+		[Test]
+		public void OutgoingAndIncoming_Byte()
+		{
+			byte[] data = new byte[256];
+			OutgoingMessage outgoingMessage = new OutgoingMessage(data);
+			for (byte value = byte.MinValue; value < byte.MaxValue; value++)
+			{
+				outgoingMessage.Write(value);
+			}
+			IncomingMessage incomingMessage = new IncomingMessage(data);
+			for (byte value = byte.MinValue; value < byte.MaxValue; value++)
+			{
+				Assert.AreEqual(value, incomingMessage.ReadByte());
+			}
+		}
+
+		[Test]
+		public void OutgoingAndIncoming_Int16()
+		{
+			byte[] data = new byte[257 * sizeof(short)];
+			OutgoingMessage outgoingMessage = new OutgoingMessage(data);
+			for (short value = short.MinValue; value < short.MaxValue; value += 255)
+			{
+				outgoingMessage.Write(value);
+			}
+			IncomingMessage incomingMessage = new IncomingMessage(data);
+			for (short value = short.MinValue; value < short.MaxValue; value += 255)
+			{
+				Assert.AreEqual(value, incomingMessage.ReadInt16());
+			}
+		}
+
+		[Test]
+		public void OutgoingAndIncoming_UInt16()
+		{
+			byte[] data = new byte[257 * sizeof(ushort)];
+			OutgoingMessage outgoingMessage = new OutgoingMessage(data);
+			for (ushort value = ushort.MinValue; value < ushort.MaxValue; value += 255)
+			{
+				outgoingMessage.Write(value);
+			}
+			IncomingMessage incomingMessage = new IncomingMessage(data);
+			for (ushort value = ushort.MinValue; value < ushort.MaxValue; value += 255)
+			{
+				Assert.AreEqual(value, incomingMessage.ReadUInt16());
+			}
+		}
+
+		[Test]
+		public void OutgoingAndIncoming_Int32()
+		{
+			byte[] data = new byte[257 * sizeof(int)];
+			OutgoingMessage outgoingMessage = new OutgoingMessage(data);
+			for (int value = int.MinValue; value < int.MaxValue; value += 16711935)
+			{
+				outgoingMessage.Write(value);
+			}
+			IncomingMessage incomingMessage = new IncomingMessage(data);
+			for (int value = int.MinValue; value < int.MaxValue; value += 16711935)
+			{
+				Assert.AreEqual(value, incomingMessage.ReadInt32());
+			}
+		}
+
+		[Test]
+		public void OutgoingAndIncoming_UInt32()
+		{
+			byte[] data = new byte[257 * sizeof(uint)];
+			OutgoingMessage outgoingMessage = new OutgoingMessage(data);
+			for (uint value = uint.MinValue; value < uint.MaxValue; value += 16711935)
+			{
+				outgoingMessage.Write(value);
+			}
+			IncomingMessage incomingMessage = new IncomingMessage(data);
+			for (uint value = uint.MinValue; value < uint.MaxValue; value += 16711935)
+			{
+				Assert.AreEqual(value, incomingMessage.ReadUInt32());
+			}
+		}
+
+		[Test]
+		public void OutgoingAndIncoming_Single()
+		{
+			byte[] data = new byte[15 * sizeof(float)];
+			OutgoingMessage outgoingMessage = new OutgoingMessage(data);
+			outgoingMessage.Write(0.0f);
+			outgoingMessage.Write(-0.0f);
+			outgoingMessage.Write(10.0f);
+			outgoingMessage.Write(-10.0f);
+			outgoingMessage.Write(6546540.3654654f);
+			outgoingMessage.Write(-957.548498f);
+			outgoingMessage.Write(-99999.87888888888f);
+			outgoingMessage.Write(0.00001f);
+			outgoingMessage.Write(-0.2f);
+			outgoingMessage.Write(Single.MinValue);
+			outgoingMessage.Write(Single.MaxValue);
+			outgoingMessage.Write(Single.NaN);
+			outgoingMessage.Write(Single.NegativeInfinity);
+			outgoingMessage.Write(Single.PositiveInfinity);
+			outgoingMessage.Write(Single.Epsilon);
+			IncomingMessage incomingMessage = new IncomingMessage(data);
+			Assert.AreEqual(0.0f, incomingMessage.ReadSingle());
+			Assert.AreEqual(-0.0f, incomingMessage.ReadSingle());
+			Assert.AreEqual(10.0f, incomingMessage.ReadSingle());
+			Assert.AreEqual(-10.0f, incomingMessage.ReadSingle());
+			Assert.AreEqual(6546540.3654654f, incomingMessage.ReadSingle());
+			Assert.AreEqual(-957.548498f, incomingMessage.ReadSingle());
+			Assert.AreEqual(-99999.87888888888f, incomingMessage.ReadSingle());
+			Assert.AreEqual(0.00001f, incomingMessage.ReadSingle());
+			Assert.AreEqual(-0.2f, incomingMessage.ReadSingle());
+			Assert.AreEqual(Single.MinValue, incomingMessage.ReadSingle());
+			Assert.AreEqual(Single.MaxValue, incomingMessage.ReadSingle());
+			Assert.AreEqual(Single.NaN, incomingMessage.ReadSingle());
+			Assert.AreEqual(Single.NegativeInfinity, incomingMessage.ReadSingle());
+			Assert.AreEqual(Single.PositiveInfinity, incomingMessage.ReadSingle());
+			Assert.AreEqual(Single.Epsilon, incomingMessage.ReadSingle());
+		}
+
+		[Test]
+		public void OutgoingAndIncoming_Boolean()
+		{
+			byte[] data = new byte[256];
+			OutgoingMessage outgoingMessage = new OutgoingMessage(data);
+			for (byte value = byte.MinValue; value < byte.MaxValue; value++)
+			{
+				outgoingMessage.Write(((value % 2) == 0));
+			}
+			IncomingMessage incomingMessage = new IncomingMessage(data);
+			for (byte value = byte.MinValue; value < byte.MaxValue; value++)
+			{
+				Assert.AreEqual(((value % 2) == 0), incomingMessage.ReadBoolean());
+			}
+		}
+
+		#endregion Tests
+	}
+}
