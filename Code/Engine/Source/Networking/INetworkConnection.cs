0fa550bd602c85f270d57b0799ba286661a8f9d9
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-09-24 10:37:23 -0400

    Moved classes to own files

---
+++ Code/Engine/Source/Networking/INetworkConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entmoot.Engine
+{
+	/// <summary>
+	/// Represents a connection between two endpoints over the network.
+	/// </summary>
+	public interface INetworkConnection
+	{
+		#region Methods
+
+		/// <summary>
+		/// Returns the next message that is coming in over the network. Returns null if no message is ready.
+		/// </summary>
+		IncomingMessage GetNextIncomingMessage();
+
+		/// <summary>
+		/// Returns an <see cref="OutgoingMessage"/> that can be sent.
+		/// </summary>
+		OutgoingMessage GetOutgoingMessageToSend();
+
+		/// <summary>
+		/// Sends the given message over the network to the other endpoint.
+		/// </summary>
+		void SendMessage(OutgoingMessage outgoingMessage);
+
+		#endregion Methods
+	}
+}
