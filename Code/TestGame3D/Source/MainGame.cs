be253055d70b33044133d1ad4fdb265be0c04acd
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-09-27 17:41:58 -0400

    Added some rendering code

--- Code/TestGame3D/Source/MainGame.cs
+++ Code/TestGame3D/Source/MainGame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Entmoot.TestGame3D
 		#region Fields
 
 		private readonly GraphicsDeviceManager graphicsDeviceManager;
+		private BasicEffect basicEffect;
 
 		#endregion Fields
 
@@ -22,6 +24,7 @@ namespace Entmoot.TestGame3D
 		public MainGame()
 		{
 			this.graphicsDeviceManager = new GraphicsDeviceManager(this);
+			this.graphicsDeviceManager.GraphicsProfile = GraphicsProfile.HiDef;
 		}
 
 		#endregion Constructors
@@ -30,6 +33,13 @@ namespace Entmoot.TestGame3D
 
 		protected override void LoadContent()
 		{
+			this.basicEffect = new BasicEffect(this.GraphicsDevice);
+			this.basicEffect.LightingEnabled = false;
+			this.basicEffect.TextureEnabled = true;
+			using (FileStream fileStream = new FileStream(@"Assets\dev_cube.png", FileMode.Open, FileAccess.Read))
+			{
+				this.basicEffect.Texture = Texture2D.FromStream(this.GraphicsDevice, fileStream);
+			}
 		}
 
 		protected override void Update(GameTime gameTime)
@@ -42,7 +52,12 @@ namespace Entmoot.TestGame3D
 
 		protected override void Draw(GameTime gameTime)
 		{
-			this.graphicsDeviceManager.GraphicsDevice.Clear(Color.Gray);
+			this.GraphicsDevice.Clear(Color.Gray);
+
+			this.basicEffect.View = Matrix.CreateLookAt(Vector3.Zero, Vector3.Forward, Vector3.Up);
+			this.basicEffect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(90.0f), this.GraphicsDevice.Viewport.AspectRatio, 0.1f, 1000.0f);
+
+			ShapeRenderHelper.DrawBox(this.GraphicsDevice, this.basicEffect, Vector3.Forward * 3, (float)gameTime.TotalGameTime.TotalSeconds);
 
 			base.Draw(gameTime);
 		}
---
+++ Code/TestGame3D/Source/ShapeRenderHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Entmoot.TestGame3D
+{
+	public static class ShapeRenderHelper
+	{
+		#region Fields
+
+		private static readonly VertexPositionTexture[] boxRenderVertices;
+
+		#endregion Fields
+
+		#region Constructors
+
+		static ShapeRenderHelper()
+		{
+			const float x0 = 0.0f;
+			const float x1 = 1.0f / 4.0f;
+			const float x2 = 2.0f / 4.0f;
+			const float x3 = 3.0f / 4.0f;
+			const float x4 = 1.0f;
+			const float y0 = 0.0f;
+			const float y1 = 1.0f / 3.0f;
+			const float y2 = 2.0f / 3.0f;
+			const float y3 = 1.0f;
+			//   2
+			// 1 3 5 6
+			//   4
+			ShapeRenderHelper.boxRenderVertices = new VertexPositionTexture[]
+			{
+				new VertexPositionTexture(new Vector3(-1, -1, 1), new Vector2(x0, y2)),
+				new VertexPositionTexture(new Vector3(-1, 1, 1), new Vector2(x0, y1)),
+				new VertexPositionTexture(new Vector3(-1, 1, -1), new Vector2(x1, y1)),
+				new VertexPositionTexture(new Vector3(-1, 1, -1), new Vector2(x1, y1)),
+				new VertexPositionTexture(new Vector3(-1, -1, -1), new Vector2(x1, y2)),
+				new VertexPositionTexture(new Vector3(-1, -1, 1), new Vector2(x0, y2)),
+
+				new VertexPositionTexture(new Vector3(-1, 1, -1), new Vector2(x1, y1)),
+				new VertexPositionTexture(new Vector3(-1, 1, 1), new Vector2(x1, y0)),
+				new VertexPositionTexture(new Vector3(1, 1, 1), new Vector2(x2, y0)),
+				new VertexPositionTexture(new Vector3(1, 1, 1), new Vector2(x2, y0)),
+				new VertexPositionTexture(new Vector3(1, 1, -1), new Vector2(x2, y1)),
+				new VertexPositionTexture(new Vector3(-1, 1, -1), new Vector2(x1, y1)),
+
+				new VertexPositionTexture(new Vector3(-1, -1, -1), new Vector2(x1, y2)),
+				new VertexPositionTexture(new Vector3(-1, 1, -1), new Vector2(x1, y1)),
+				new VertexPositionTexture(new Vector3(1, 1, -1), new Vector2(x2, y1)),
+				new VertexPositionTexture(new Vector3(1, 1, -1), new Vector2(x2, y1)),
+				new VertexPositionTexture(new Vector3(1, -1, -1), new Vector2(x2, y2)),
+				new VertexPositionTexture(new Vector3(-1, -1, -1), new Vector2(x1, y2)),
+
+				new VertexPositionTexture(new Vector3(-1, -1, 1), new Vector2(x1, y3)),
+				new VertexPositionTexture(new Vector3(-1, -1, -1), new Vector2(x1, y2)),
+				new VertexPositionTexture(new Vector3(1, -1, -1), new Vector2(x2, y2)),
+				new VertexPositionTexture(new Vector3(1, -1, -1), new Vector2(x2, y2)),
+				new VertexPositionTexture(new Vector3(1, -1, 1), new Vector2(x2, y3)),
+				new VertexPositionTexture(new Vector3(-1, -1, 1), new Vector2(x1, y3)),
+
+				new VertexPositionTexture(new Vector3(1, -1, -1), new Vector2(x2, y2)),
+				new VertexPositionTexture(new Vector3(1, 1, -1), new Vector2(x2, y1)),
+				new VertexPositionTexture(new Vector3(1, 1, 1), new Vector2(x3, y1)),
+				new VertexPositionTexture(new Vector3(1, 1, 1), new Vector2(x3, y1)),
+				new VertexPositionTexture(new Vector3(1, -1, 1), new Vector2(x3, y2)),
+				new VertexPositionTexture(new Vector3(1, -1, -1), new Vector2(x2, y2)),
+
+				new VertexPositionTexture(new Vector3(1, -1, 1), new Vector2(x3, y2)),
+				new VertexPositionTexture(new Vector3(1, 1, 1), new Vector2(x3, y1)),
+				new VertexPositionTexture(new Vector3(-1, 1, 1), new Vector2(x4, y1)),
+				new VertexPositionTexture(new Vector3(-1, 1, 1), new Vector2(x4, y1)),
+				new VertexPositionTexture(new Vector3(-1, -1, 1), new Vector2(x4, y2)),
+				new VertexPositionTexture(new Vector3(1, -1, 1), new Vector2(x3, y2)),
+			};
+		}
+
+		#endregion Constructors
+
+		#region Methods
+
+		public static void DrawBox(GraphicsDevice graphicsDevice, BasicEffect basicEffect, Vector3 position, float rotation)
+		{
+			basicEffect.World = Matrix.CreateRotationY(rotation) * Matrix.CreateTranslation(position);
+			basicEffect.CurrentTechnique.Passes[0].Apply();
+			graphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, ShapeRenderHelper.boxRenderVertices, 0, ShapeRenderHelper.boxRenderVertices.Length / 3);
+
+		}
+
+		#endregion Methods
+	}
+}
