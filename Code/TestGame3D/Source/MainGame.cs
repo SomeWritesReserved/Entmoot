45e869f341b7b032d24c593005aacdbd14dc2b1e
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-09-26 23:37:04 -0400

    Added barebones TestGame3D project

---
+++ Code/TestGame3D/Source/MainGame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Entmoot.TestGame3D
+{
+	public class MainGame : Game
+	{
+		#region Fields
+
+		private readonly GraphicsDeviceManager graphicsDeviceManager;
+
+		#endregion Fields
+
+		#region Constructors
+
+		public MainGame()
+		{
+			this.graphicsDeviceManager = new GraphicsDeviceManager(this);
+		}
+
+		#endregion Constructors
+
+		#region Methods
+
+		protected override void LoadContent()
+		{
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			KeyboardState keyboardState = Keyboard.GetState();
+			if (keyboardState.IsKeyDown(Keys.Escape)) { this.Exit(); }
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			this.graphicsDeviceManager.GraphicsDevice.Clear(Color.Gray);
+
+			base.Draw(gameTime);
+		}
+
+		#endregion Methods
+	}
+}
---
+++ Code/TestGame3D/Source/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entmoot.TestGame3D
+{
+	public static class Program
+	{
+		#region Methods
+
+		[STAThread]
+		public static void Main()
+		{
+			using (MainGame game = new MainGame())
+			{
+				game.Run();
+			}
+		}
+
+		#endregion Methods
+	}
+}
