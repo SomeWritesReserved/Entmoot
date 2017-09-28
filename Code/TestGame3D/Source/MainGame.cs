8980439fd1865fd20102be0b113fc083f385cb1d
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-09-27 21:51:35 -0400

    Changed TestGame3D to NetworkClient and NetworkServer instead of ListenServer

--- Code/TestGame3D/Source/MainGame.cs
+++ Code/TestGame3D/Source/MainGame.cs
@@ -15,11 +15,18 @@ namespace Entmoot.TestGame3D
 	{
 		#region Fields
 
+		private const int maxClients = 4;
+
 		private readonly GraphicsDeviceManager graphicsDeviceManager;
-		private readonly RenderSystem renderSystem;
-		private readonly ListenServer<CommandData> listenServer;
-		private CommandData commandData = new CommandData();
 		private BasicEffect basicEffect;
+
+		private readonly bool hasServer;
+		private readonly RenderSystem renderSystem;
+		private readonly NetworkServer networkServer;
+		private readonly NetworkClient networkClient;
+		private readonly GameServer<CommandData> gameServer;
+		private readonly GameClient<CommandData> gameClient;
+		private CommandData commandData = new CommandData();
 
 		#endregion Fields
 
@@ -29,32 +36,69 @@ namespace Entmoot.TestGame3D
 		{
 			this.graphicsDeviceManager = new GraphicsDeviceManager(this);
 			this.graphicsDeviceManager.GraphicsProfile = GraphicsProfile.HiDef;
+			this.renderSystem = new RenderSystem(this.graphicsDeviceManager);
 
 			ComponentsDefinition componentsDefinition = new ComponentsDefinition();
 			componentsDefinition.RegisterComponentType<SpatialComponent>();
 
-			this.renderSystem = new RenderSystem(this.graphicsDeviceManager);
-			this.listenServer = new ListenServer<CommandData>(4000, 20, 100, componentsDefinition, new ISystem[0], new ISystem[] { this.renderSystem });
-			this.listenServer.GameClient.NetworkSendRate = 1;
-			this.listenServer.GameClient.InterpolationRenderDelay = 3;
-			this.listenServer.GameServer.NetworkSendRate = 1;
-
-			this.listenServer.GameServer.EntityArray.TryCreateEntity(out Entity clientEntity);
-			clientEntity.AddComponent<SpatialComponent>();
-
-			for (int x = -2; x <= 2; x++)
+			this.hasServer = Environment.GetCommandLineArgs().Any((arg) => arg.Equals("-s", StringComparison.OrdinalIgnoreCase));
+			if (this.hasServer)
 			{
-				for (int z = -2; z <= 2; z++)
+				this.networkServer = new NetworkServer("1", MainGame.maxClients, 4000, 19876);
+				this.gameServer = new GameServer<CommandData>(this.networkServer.ClientNetworkConnections, 20, 30, componentsDefinition, new ISystem[0]);
+
+				// Reserve the first entities for all potential clients
+				for (int clientID = 0; clientID < MainGame.maxClients; clientID++)
 				{
-					this.listenServer.GameServer.EntityArray.TryCreateEntity(out Entity entity);
-					entity.AddComponent<SpatialComponent>().Position = new Vector3(x * 5, 0, z * 5);
+					this.gameServer.EntityArray.TryCreateEntity(out Entity clientEntity);
+					clientEntity.AddComponent<SpatialComponent>();
+				}
+
+				// Add some stuff to the world
+				for (int x = -2; x <= 2; x++)
+				{
+					for (int z = -2; z <= 2; z++)
+					{
+						this.gameServer.EntityArray.TryCreateEntity(out Entity entity);
+						entity.AddComponent<SpatialComponent>().Position = new Vector3(x * 5, 0, z * 5);
+					}
 				}
 			}
+
+			this.networkClient = new NetworkClient("1", 4000);
+			this.gameClient = new GameClient<CommandData>(this.networkClient, 20, 30, componentsDefinition, new ISystem[] { this.renderSystem });
 		}
 
 		#endregion Constructors
 
 		#region Methods
+
+		protected override void OnActivated(object sender, EventArgs args)
+		{
+			Mouse.SetPosition(this.GraphicsDevice.Viewport.Width / 2, this.GraphicsDevice.Viewport.Height / 2);
+			base.OnActivated(sender, args);
+		}
+
+		protected override void OnExiting(object sender, EventArgs args)
+		{
+			this.networkClient.Disconnect();
+			if (this.hasServer)
+			{
+				this.networkServer.Stop();
+			}
+			base.OnExiting(sender, args);
+		}
+
+		protected override void Initialize()
+		{
+			if (this.hasServer)
+			{
+				this.networkServer.Start();
+			}
+			this.networkClient.Connect(new System.Net.IPEndPoint(System.Net.IPAddress.Loopback, 19876));
+
+			base.Initialize();
+		}
 
 		protected override void LoadContent()
 		{
@@ -66,13 +110,10 @@ namespace Entmoot.TestGame3D
 			{
 				this.basicEffect.Texture = Texture2D.FromStream(this.GraphicsDevice, fileStream);
 			}
-
-			Mouse.SetPosition(this.GraphicsDevice.Viewport.Width / 2, this.GraphicsDevice.Viewport.Height / 2);
 		}
 
 		protected override void Update(GameTime gameTime)
 		{
-			base.Update(gameTime);
 		}
 
 		protected override void Draw(GameTime gameTime)
@@ -83,13 +124,23 @@ namespace Entmoot.TestGame3D
 			this.basicEffect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(90.0f), this.GraphicsDevice.Viewport.AspectRatio, 0.1f, 1000.0f);
 			this.renderSystem.BasicEffect = this.basicEffect;
 
+			if (this.hasServer)
+			{
+				this.networkServer.Update();
+				this.gameServer.Update();
+			}
+
 			MouseState mouseState = Mouse.GetState();
 			KeyboardState keyboardState = Keyboard.GetState();
 			{
-				float deltaX = (this.GraphicsDevice.Viewport.Width / 2 - mouseState.X) * 0.004f;
-				float deltaY = (this.GraphicsDevice.Viewport.Height / 2 - mouseState.Y) * 0.004f;
-				this.commandData.LookAngles.X += deltaX;
-				this.commandData.LookAngles.Y = MathHelper.Clamp(this.commandData.LookAngles.Y + deltaY, -MathHelper.Pi * 0.49f, MathHelper.Pi * 0.49f);
+				if (this.IsActive && mouseState.RightButton == ButtonState.Pressed)
+				{
+					float deltaX = (this.GraphicsDevice.Viewport.Width / 2 - mouseState.X) * 0.004f;
+					float deltaY = (this.GraphicsDevice.Viewport.Height / 2 - mouseState.Y) * 0.004f;
+					this.commandData.LookAngles.X += deltaX;
+					this.commandData.LookAngles.Y = MathHelper.Clamp(this.commandData.LookAngles.Y + deltaY, -MathHelper.Pi * 0.49f, MathHelper.Pi * 0.49f);
+					Mouse.SetPosition(this.GraphicsDevice.Viewport.Width / 2, this.GraphicsDevice.Viewport.Height / 2);
+				}
 
 				this.commandData.Commands = Commands.None;
 				if (keyboardState.IsKeyDown(Keys.W)) { this.commandData.Commands |= Commands.MoveForward; }
@@ -97,7 +148,7 @@ namespace Entmoot.TestGame3D
 				if (keyboardState.IsKeyDown(Keys.A)) { this.commandData.Commands |= Commands.MoveLeft; }
 				if (keyboardState.IsKeyDown(Keys.D)) { this.commandData.Commands |= Commands.MoveRight; }
 
-				if (this.listenServer.GameClient.HasRenderingStarted && this.listenServer.GameClient.RenderedSnapshot.EntityArray.TryGetEntity(this.listenServer.GameClient.CommandingEntityID, out Entity clientEntity))
+				if (this.gameClient.HasRenderingStarted && this.gameClient.RenderedSnapshot.EntityArray.TryGetEntity(this.gameClient.CommandingEntityID, out Entity clientEntity))
 				{
 					if (clientEntity.HasComponent<SpatialComponent>())
 					{
@@ -105,11 +156,10 @@ namespace Entmoot.TestGame3D
 						this.basicEffect.View = Matrix.CreateLookAt(spatialComponent.Position, spatialComponent.Position + Vector3.Transform(Vector3.Forward, spatialComponent.Rotation), Vector3.Up);
 					}
 				}
-
-				this.listenServer.Update(this.commandData);
+				this.networkClient.Update();
+				this.gameClient.Update(this.commandData);
 			}
 
-			Mouse.SetPosition(this.GraphicsDevice.Viewport.Width / 2, this.GraphicsDevice.Viewport.Height / 2);
 			if (keyboardState.IsKeyDown(Keys.Escape)) { this.Exit(); }
 		}
 
