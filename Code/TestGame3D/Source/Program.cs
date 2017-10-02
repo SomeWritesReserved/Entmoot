0d90e39a3a3c94512438f3002263afc71aed7d32
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-10-01 23:45:18 -0400

    TestGame3D: Added client to dedicated server mode for headless testing of GC

--- Code/TestGame3D/Source/Program.cs
+++ Code/TestGame3D/Source/Program.cs
@@ -26,47 +26,54 @@ namespace Entmoot.TestGame3D
 				componentsDefinition.RegisterComponentType<ColorComponent>();
 				NetworkServer networkServer = new NetworkServer("1", maxClient, 4000, 19876);
 				GameServer<CommandData> gameServer = new GameServer<CommandData>(networkServer.ClientNetworkConnections, 20, 30, componentsDefinition, new ISystem[] { new SpinnerSystem() });
-				// Reserve the first entities for all potential clients
-				for (int clientID = 0; clientID < maxClient; clientID++)
 				{
-					gameServer.EntityArray.TryCreateEntity(out Entity clientEntity);
-					clientEntity.AddComponent<SpatialComponent>();
-					clientEntity.AddComponent<ColorComponent>().Color = new Color(0.5f, 0.5f, 1.0f);
-				}
-
-				// Make some dummy entities that we'll remove to have a gap in entity IDs
-				gameServer.EntityArray.TryCreateEntity(out Entity dummy1);
-				gameServer.EntityArray.TryCreateEntity(out Entity dummy2);
-
-				// Add some stuff to the world
-				for (int x = -1; x <= 1; x++)
-				{
-					for (int z = -1; z <= 1; z++)
+					// Reserve the first entities for all potential clients
+					for (int clientID = 0; clientID < maxClient; clientID++)
 					{
-						gameServer.EntityArray.TryCreateEntity(out Entity entity);
-						entity.AddComponent<SpatialComponent>().Position = new Vector3(x * 5, 0, z * 5);
-						if (entity.ID == 11)
+						gameServer.EntityArray.TryCreateEntity(out Entity clientEntity);
+						clientEntity.AddComponent<SpatialComponent>();
+						clientEntity.AddComponent<ColorComponent>().Color = new Color(0.5f, 0.5f, 1.0f);
+					}
+
+					// Make some dummy entities that we'll remove to have a gap in entity IDs
+					gameServer.EntityArray.TryCreateEntity(out Entity dummy1);
+					gameServer.EntityArray.TryCreateEntity(out Entity dummy2);
+
+					// Add some stuff to the world
+					for (int x = -1; x <= 1; x++)
+					{
+						for (int z = -1; z <= 1; z++)
 						{
-							entity.AddComponent<ColorComponent>().Color = new Color(1.0f, 0.5f, 0.5f);
-						}
-						else if (entity.ID == 12)
-						{
-							entity.AddComponent<ColorComponent>().Color = new Color(0.0f, 1.0f, 0.5f);
+							gameServer.EntityArray.TryCreateEntity(out Entity entity);
+							entity.AddComponent<SpatialComponent>().Position = new Vector3(x * 5, 0, z * 5);
+							if (entity.ID == 11)
+							{
+								entity.AddComponent<ColorComponent>().Color = new Color(1.0f, 0.5f, 0.5f);
+							}
+							else if (entity.ID == 12)
+							{
+								entity.AddComponent<ColorComponent>().Color = new Color(0.0f, 1.0f, 0.5f);
+							}
 						}
 					}
+
+					// Remove those dummy entities to have a gap in entity IDs
+					gameServer.EntityArray.RemoveEntity(dummy1);
+					gameServer.EntityArray.RemoveEntity(dummy2);
+					gameServer.EntityArray.EndUpdate();
+					networkServer.Start();
 				}
 
-				// Remove those dummy entities to have a gap in entity IDs
-				gameServer.EntityArray.RemoveEntity(dummy1);
-				gameServer.EntityArray.RemoveEntity(dummy2);
-				gameServer.EntityArray.EndUpdate();
-				networkServer.Start();
+				NetworkClient networkClient = new NetworkClient("1", 4000);
+				GameClient<CommandData> gameClient = new GameClient<CommandData>(networkClient, 20, 30, componentsDefinition, new ISystem[] { });
+				{
+					networkClient.Connect(new System.Net.IPEndPoint(System.Net.IPAddress.Loopback, 19876));
+				}
+
 				GC.Collect(10, GCCollectionMode.Forced, true, true);
 
-				Program.runDedicatedServer(networkServer, gameServer);
-				networkServer.Stop();
+				Program.runDedicatedServer(networkServer, gameServer, networkClient, gameClient);
 
-				System.Threading.Thread.Sleep(250);
 			}
 			else
 			{
@@ -77,16 +84,36 @@ namespace Entmoot.TestGame3D
 			}
 		}
 
-		private static void runDedicatedServer(NetworkServer networkServer, GameServer<CommandData> gameServer)
+		private static void runDedicatedServer(NetworkServer networkServer, GameServer<CommandData> gameServer, NetworkClient networkClient, GameClient<CommandData> gameClient)
 		{
 			int tick = 0;
 			while (true)
 			{
-				networkServer.Update();
-				gameServer.Update();
+				Program.runServerTick(networkServer, gameServer);
+				Program.runClientTick(networkClient, gameClient);
 				System.Threading.Thread.Sleep(15);
 				tick++;
+				if (tick == 600) { break; }
 			}
+
+			networkClient.Disconnect();
+			System.Threading.Thread.Sleep(250);
+			networkServer.Stop();
+			System.Threading.Thread.Sleep(250);
+		}
+
+		[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+		private static void runServerTick(NetworkServer networkServer, GameServer<CommandData> gameServer)
+		{
+			networkServer.Update();
+			gameServer.Update();
+		}
+
+		[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+		private static void runClientTick(NetworkClient networkClient, GameClient<CommandData> gameClient)
+		{
+			networkClient.Update();
+			gameClient.Update(new CommandData());
 		}
 
 		#endregion Methods
