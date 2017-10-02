f9dc43a8770d7e98a905fc835dab756797e2b86a
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2017-10-01 20:34:05 -0400

    TestGame3D: Added code for psuedo-dedicated server
    This is for testing purposes only and doesn't server as a real example
    on how to do a dedicated server. Instead it exists to measure
    allocations in a sandboxed environment (no WinForms or XNA).

--- Code/TestGame3D/Source/Program.cs
+++ Code/TestGame3D/Source/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Entmoot.Engine;
+using Microsoft.Xna.Framework;
 
 namespace Entmoot.TestGame3D
 {
@@ -11,11 +14,78 @@ namespace Entmoot.TestGame3D
 		#region Methods
 
 		[STAThread]
-		public static void Main()
+		public static void Main(string[] args)
 		{
-			using (MainGame game = new MainGame())
+			GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
+
+			if (args.Any((arg) => arg.Equals("-ds", StringComparison.OrdinalIgnoreCase)))
 			{
-				game.Run();
+				const int maxClient = 4;
+				ComponentsDefinition componentsDefinition = new ComponentsDefinition();
+				componentsDefinition.RegisterComponentType<SpatialComponent>();
+				componentsDefinition.RegisterComponentType<ColorComponent>();
+				NetworkServer networkServer = new NetworkServer("1", maxClient, 4000, 19876);
+				GameServer<CommandData> gameServer = new GameServer<CommandData>(networkServer.ClientNetworkConnections, 20, 30, componentsDefinition, new ISystem[] { new SpinnerSystem() });
+				// Reserve the first entities for all potential clients
+				for (int clientID = 0; clientID < maxClient; clientID++)
+				{
+					gameServer.EntityArray.TryCreateEntity(out Entity clientEntity);
+					clientEntity.AddComponent<SpatialComponent>();
+					clientEntity.AddComponent<ColorComponent>().Color = new Color(0.5f, 0.5f, 1.0f);
+				}
+
+				// Make some dummy entities that we'll remove to have a gap in entity IDs
+				gameServer.EntityArray.TryCreateEntity(out Entity dummy1);
+				gameServer.EntityArray.TryCreateEntity(out Entity dummy2);
+
+				// Add some stuff to the world
+				for (int x = -1; x <= 1; x++)
+				{
+					for (int z = -1; z <= 1; z++)
+					{
+						gameServer.EntityArray.TryCreateEntity(out Entity entity);
+						entity.AddComponent<SpatialComponent>().Position = new Vector3(x * 5, 0, z * 5);
+						if (entity.ID == 11)
+						{
+							entity.AddComponent<ColorComponent>().Color = new Color(1.0f, 0.5f, 0.5f);
+						}
+						else if (entity.ID == 12)
+						{
+							entity.AddComponent<ColorComponent>().Color = new Color(0.0f, 1.0f, 0.5f);
+						}
+					}
+				}
+
+				// Remove those dummy entities to have a gap in entity IDs
+				gameServer.EntityArray.RemoveEntity(dummy1);
+				gameServer.EntityArray.RemoveEntity(dummy2);
+				gameServer.EntityArray.EndUpdate();
+				networkServer.Start();
+				GC.Collect(10, GCCollectionMode.Forced, true, true);
+
+				Program.runDedicatedServer(networkServer, gameServer);
+				networkServer.Stop();
+
+				System.Threading.Thread.Sleep(250);
+			}
+			else
+			{
+				using (MainGame game = new MainGame())
+				{
+					game.Run();
+				}
+			}
+		}
+
+		private static void runDedicatedServer(NetworkServer networkServer, GameServer<CommandData> gameServer)
+		{
+			int tick = 0;
+			while (true)
+			{
+				networkServer.Update();
+				gameServer.Update();
+				System.Threading.Thread.Sleep(15);
+				tick++;
 			}
 		}
 
