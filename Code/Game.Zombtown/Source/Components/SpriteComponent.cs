3ae5dfee72b4696059e8f3eb51e41b1224a2f154
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2020-08-11 19:28:55 -0400

    Zombtown: 2D rendering working with Rendering2dSystem and CameraComponent.
    The Rendering2dSystem will take world space sprites (with position,
    rotation, and radius), scale textures to fit the world space, then
    convert the world space coordinates into screen space coordinates (for
    use with XNA's SpriteBatch). To determine what to show on screen the
    commanding entity is expected to have a CameraComponent to determine
    what to show. The CameraSystem exists server side simply to follow the
    player but could be used for more advanced things (like lerping).
    
    XNA's SpriteBatch has a quirk were rotation happens _after_ scaling,
    which means if a sprite is squished because of incorrect aspect ratio
    then the rotation rotates the squished-ness (instead of it rotating then
    being squished). Additional weirdness comes from specifying the
    texture's origin, which we do so rotations happens about the center:
    when we change the origin we need to offset the destination rectangle so
    it starts in the center too.

---
+++ Code/Game.Zombtown/Source/Components/SpriteComponent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entmoot.Engine;
+
+namespace Entmoot.Game.Zombtown
+{
+	public struct SpriteComponent : IComponent<SpriteComponent>
+	{
+		#region Fields
+
+		public int SpriteId;
+
+		#endregion Fields
+
+		#region Methods
+
+		public bool Equals(SpriteComponent other)
+		{
+			return (this.SpriteId == other.SpriteId);
+		}
+
+		public void Interpolate(SpriteComponent otherA, SpriteComponent otherB, float amount)
+		{
+			this.SpriteId = otherB.SpriteId;
+		}
+
+		public void Serialize(IWriter writer)
+		{
+			writer.Write(this.SpriteId);
+		}
+
+		public void Deserialize(IReader reader)
+		{
+			this.SpriteId = reader.ReadInt32();
+		}
+
+		public void ResetToDefaults()
+		{
+			this.SpriteId = 0;
+		}
+
+		public void SetSprite(string name)
+		{
+			this.SpriteId = name.GetHashCode();
+		}
+
+		#endregion Methods
+	}
+}
--- Code/Game.Zombtown/Source/Systems/Render2dSystem.cs
+++ Code/Game.Zombtown/Source/Systems/Render2dSystem.cs
@@ -11,6 +11,13 @@ namespace Entmoot.Game.Zombtown
 {
 	public class Render2dSystem : IClientSystem
 	{
+		#region Fields
+
+		private readonly Dictionary<int, string> textureIdToName = new Dictionary<int, string>();
+		private readonly Dictionary<int, Texture2D> textures = new Dictionary<int, Texture2D>();
+
+		#endregion Fields
+
 		#region Constructors
 
 		public Render2dSystem(GraphicsDeviceManager graphicsDeviceManager)
@@ -24,11 +31,24 @@ namespace Entmoot.Game.Zombtown
 
 		public GraphicsDeviceManager GraphicsDeviceManager { get; }
 
-		public BasicEffect BasicEffect { get; set; }
+		public SpriteBatch SpriteBatch { get; set; }
 
 		#endregion Properties
 
 		#region Methods
+
+		public void Clear()
+		{
+			this.textureIdToName.Clear();
+			this.textures.Clear();
+		}
+
+		public void AddTexture(string name, Texture2D texture)
+		{
+			int id = name.GetHashCode();
+			textureIdToName.Add(id, name);
+			textures.Add(id, texture);
+		}
 
 		public void ClientUpdate(EntityArray entityArray, Entity commandingEntity)
 		{
@@ -40,6 +60,87 @@ namespace Entmoot.Game.Zombtown
 
 		public void ClientRender(EntityArray entityArray, Entity commandingEntity)
 		{
+			if (!commandingEntity.IsValid || !commandingEntity.HasComponent<SpatialComponent>()) { return; }
+
+			CameraComponent playerCameraComponent = commandingEntity.GetComponent<CameraComponent>();
+
+			this.SpriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.NonPremultiplied);
+			foreach (Entity entity in entityArray)
+			{
+				if (!entity.HasComponent<SpatialComponent>() || !entity.HasComponent<SpriteComponent>()) { continue; }
+
+				SpatialComponent spatialComponent = entity.GetComponent<SpatialComponent>();
+				SpriteComponent spriteComponent = entity.GetComponent<SpriteComponent>();
+
+				if (spatialComponent.Radius == 0) { continue; }
+				if (spriteComponent.SpriteId == 0 || !this.textures.TryGetValue(spriteComponent.SpriteId, out Texture2D spriteTexture)) { continue; }
+
+				this.renderSprite(spriteTexture, spatialComponent.Position, spatialComponent.Radius, spatialComponent.Rotation,
+					playerCameraComponent.Position, playerCameraComponent.Extents);
+			}
+			this.SpriteBatch.End();
+		}
+
+		/// <summary>
+		/// Renders a sprite in a specific world location based on the specified camera view.
+		/// </summary>
+		/// <remarks>
+		/// The size or dimensions of the texture does not matter, it will be scaled to fit into the sprites world position and rotated.
+		/// Note: it is critical that the camera's aspect ratio match the render target's aspect ratios, otherwise the scaling and rotation will be wrong.
+		/// </remarks>
+		private void renderSprite(Texture2D texture, Vector2 spriteWorldPosition, float spriteWorldSize, float spriteWorldRotation, Vector2 cameraWorldPosition, Vector2 cameraWorldExtents)
+		{
+			// Get the dimensions of the camera in both screen and world space so we can transform between them.
+			float cameraWorldWidth = cameraWorldExtents.X * 2.0f;
+			float cameraWorldHeight = cameraWorldExtents.Y * 2.0f;
+			float cameraScreenWidth = this.GraphicsDeviceManager.GraphicsDevice.Viewport.Width;
+			float cameraScreenHeight = this.GraphicsDeviceManager.GraphicsDevice.Viewport.Height;
+
+			// Get world positions of the camera's rectangle and the sprite's rectangle.
+			// Use top-left and bottom-right (instead of bottom-left and top-right) as the rectangle extents to match the
+			// inverted Y axis of screen space (where 0,0 is the top-left corner and 1,1 is the bottom-right).
+			Vector2 cameraWorldTopLeft = new Vector2(
+				cameraWorldPosition.X - cameraWorldExtents.X,
+				cameraWorldPosition.Y + cameraWorldExtents.Y);
+			Vector2 cameraWorldBottomRight = new Vector2(
+				cameraWorldPosition.X + cameraWorldExtents.X,
+				cameraWorldPosition.Y - cameraWorldExtents.Y);
+
+			Vector2 spriteWorldTopLeft = new Vector2(
+				spriteWorldPosition.X - spriteWorldSize,
+				spriteWorldPosition.Y + spriteWorldSize);
+			Vector2 spriteWorldCenter = new Vector2(
+				spriteWorldPosition.X,
+				spriteWorldPosition.Y);
+			Vector2 spriteWorldBottomRight = new Vector2(
+				spriteWorldPosition.X + spriteWorldSize,
+				spriteWorldPosition.Y - spriteWorldSize);
+
+			// Convert world positions to screen positions for the sprite based on the ratio between screen size and camera size, and the camera's world position.
+			Vector2 spriteScreenTopLeft = new Vector2(
+				(spriteWorldTopLeft.X - cameraWorldTopLeft.X) * (cameraScreenWidth / cameraWorldWidth),
+				-(spriteWorldTopLeft.Y - cameraWorldTopLeft.Y) * (cameraScreenHeight / cameraWorldHeight));
+			Vector2 spriteScreenCenter = new Vector2(
+				(spriteWorldCenter.X - cameraWorldTopLeft.X) * (cameraScreenWidth / cameraWorldWidth),
+				-(spriteWorldCenter.Y - cameraWorldTopLeft.Y) * (cameraScreenHeight / cameraWorldHeight));
+			Vector2 spriteScreenBottomRight = new Vector2(
+				(spriteWorldBottomRight.X - cameraWorldTopLeft.X) * (cameraScreenWidth / cameraWorldWidth),
+				-(spriteWorldBottomRight.Y - cameraWorldTopLeft.Y) * (cameraScreenHeight / cameraWorldHeight));
+
+			// SpriteBatch is weird because changing the origin changes how translation is applied. You would expect the destination
+			// rectangle to be the top-left and bottom-right corners but because we change the origin to be the center of the texture
+			// we need to specify the destination rectangle as starting in the center of the sprite.
+			// I'm not sure why this is, I would expect to specify the top-left, but this is how SpriteBatch appears to work.
+			Rectangle spriteScreenRectangle = new Rectangle(
+				(int)Math.Round(spriteScreenCenter.X),
+				(int)Math.Round(spriteScreenCenter.Y),
+				(int)Math.Round(spriteScreenBottomRight.X - spriteScreenTopLeft.X),
+				(int)Math.Round(spriteScreenBottomRight.Y - spriteScreenTopLeft.Y));
+
+			// After all this calculation it doesn't even work because rotations happen _after_ the scaling, so the scaled sprite is rotated causing the elongated
+			// axis to spin. This means the aspect ratio of the camera must match the screen.
+			Vector2 textureOrigin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
+			this.SpriteBatch.Draw(texture, spriteScreenRectangle, null, Color.White, spriteWorldRotation, textureOrigin, SpriteEffects.None, 0);
 		}
 
 		#endregion Methods
